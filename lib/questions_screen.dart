0b7ca3794e0b5f97db86700b1eb5c342bb8101f3
thipphimon <thipphimonsawangrat@gmail.com>
2025-02-14 11:10:52 +0700

    Update questions_screen.dart

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
 import 'answer_button.dart';
+import 'results_screen.dart';
+import 'data/quizz.dart';
 
-class QuestionsScreen extends StatelessWidget {
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
   @override
+  State<QuestionsScreen> createState() => _QuestionsScreenState();
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  int currentQuestionIndex = 0;
+  List<String> selectedAnswers = [];
+  int correctAnswersCount = 0;
+
+  void selectAnswer(String answer) {
+    setState(() {
+      selectedAnswers.add(answer);
+      if (answer == questions[currentQuestionIndex].correctAnswer) {
+        correctAnswersCount++;
+      }
+      if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+      } else {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ResultsScreen(
+              selectedAnswers: selectedAnswers,
+              correctAnswersCount: correctAnswersCount,
+              totalQuestions: questions.length,
+            ),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Quiz Questions'),
@@ -24,22 +59,22 @@ class QuestionsScreen extends StatelessWidget {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  const Text(
-                    'What are the main building blocks of Flutter UIs?',
-                    style: TextStyle(
+                  Text(
+                    currentQuestion.question,
+                    style: const TextStyle(
                       color: Colors.white,
                       fontSize: 18,
                     ),
                     textAlign: TextAlign.center,
                   ),
                   const SizedBox(height: 30),
-                  AnswerButton('Functions'),
+                  ...currentQuestion.answers.map(
+                    (answer) => AnswerButton(
+                      answer,
+                      onPressed: () => selectAnswer(answer),
+                    ),
+                  ),
                   const SizedBox(height: 10),
-                  AnswerButton('Components'),
-                  const SizedBox(height: 10),
-                  AnswerButton('Blocks'),
-                  const SizedBox(height: 10),
-                  AnswerButton('Widgets'),
                 ],
               ),
             ),
