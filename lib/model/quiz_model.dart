5713b32d9e3c8eda5f8ee1ed359c0b9f08d2125a
thipphimon <thipphimonsawangrat@gmail.com>
2025-02-14 11:09:52 +0700

    Update quiz_model.dart

--- lib/model/quiz_model.dart
+++ lib/model/quiz_model.dart
@@ -1,6 +1,13 @@
 class QuizModel {
   String question;
   List<String> answers;
+  String correctAnswer;
 
-  QuizModel(this.question, this.answers);
+  QuizModel(this.question, this.answers, this.correctAnswer);
+
+  List<String> getShuffledAnswers() {
+    final shuffledAnswers = List<String>.from(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }
