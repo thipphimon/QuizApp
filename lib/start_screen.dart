4f8f9028759fbdc34f246c0e95c408b6172bf380
thipphimon <thipphimonsawangrat@gmail.com>
2025-02-14 11:11:22 +0700

    Update start_screen.dart

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'questions_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -17,17 +18,24 @@ class StartScreen extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 300,
+              Opacity(
+                opacity: 0.8,
+                child: Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                ),
               ),
               const SizedBox(height: 30),
-              const Text(
-                'Learn flutter the fun way!',
-                style: TextStyle(color: Colors.white),
+              Text(
+                'Learn Flutter the fun way!',
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               const SizedBox(height: 30),
-              OutlinedButton(
+              OutlinedButton.icon(
                 onPressed: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
@@ -35,7 +43,8 @@ class StartScreen extends StatelessWidget {
                     ),
                   );
                 },
-                child: const Text(
+                icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
+                label: const Text(
                   'Start Quiz',
                   style: TextStyle(color: Colors.white),
                 ),
